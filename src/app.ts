ff311e53542f3f1e453e7c77c5ea4c1116f065af
Rafioul Hasan <rafioulhasan2@gmail.com>
2025-07-17 19:42:08 +0600

    Initialize FitFlex backend with TypeScript, Express, and Prisma setup

---
+++ src/app.ts
@@ -0,0 +1,39 @@
+import express, { Application, NextFunction, Request, Response } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+const app: Application = express();
+import status from "http-status";
+
+app.use(
+  cors({
+    origin: "https://greeni-circle-client.vercel.app",
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+
+// parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send({
+    Message: "Green Circle Server Is Running",
+  });
+});
+// all routes
+// app.use("/api", router);
+
+// global error handler
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(status.NOT_FOUND).json({
+    success: false,
+    message: "API not found",
+    error: {
+      path: req.originalUrl,
+      message: "Your requested path is not found",
+    },
+  });
+});
+
+export default app;
